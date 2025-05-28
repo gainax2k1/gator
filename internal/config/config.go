aacfed7faf912556f96be8fbdd9516d47d742aab
gainax2k1 <gainax2k1@gmail.com>
2025-05-28 03:03:33 -0700

    added config file functionality

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,23 +1,81 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
 
 const configFileName = ".gatorconfig.json"
 
-// todo: export aconfig struct  representing json structure with tags
-
-// export read function  reads the json file at ~/.gatorconfig.json returns Config struct
-// --should read from the home directory, then decode the json string into a new config struct
-// --use "os.UserHomeDir" to get location of home
+type Config struct { // -export aconfig struct  representing json structure with tags
+	DbURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
 
 // export a "SetUser" method on the "Config" struct that writes the config struct to the  JSON file
 // after setting "current_user_name" field
+func (c *Config) SetUser(username string) error {
+	c.CurrentUserName = username
+	err := writeGatorConfig(*c)
+	if err != nil {
+		return err
+	}
 
+	return nil
+}
 
-//helper functions:
-func getConfigFilePath() (string, error){
+// export read function  reads the json file at ~/.gatorconfig.json returns Config struct
+
+func Read() (Config, error) {
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	gatorJson, err := os.ReadFile(fullPath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var gatorConfig Config
+
+	err = json.Unmarshal(gatorJson, &gatorConfig)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return gatorConfig, nil
 
 }
 
-func write(cfg Config) error {
+// helper functions:
+func getConfigFilePath() (string, error) {
+	homePath, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
 
+	fullPath := filepath.Join(homePath, configFileName)
+	return fullPath, nil
+
+}
+
+func writeGatorConfig(cfg Config) error {
+	gatorJson, err := json.Marshal(cfg)
+
+	if err != nil {
+		return err
+	}
+
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(fullPath, gatorJson, 0666)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main(){
-	fmt.Println("hello gator world")
+	"github.com/gainax2k1/gator/internal/config"
+)
+
+func main() {
+
+	userConfig, err := config.Read()
+	if err != nil {
+		fmt.Println("Error reading config: ", err)
+	}
+
+	userConfig.SetUser("Rico")
+	userConfig, err = config.Read()
+	if err != nil {
+		fmt.Println("Error reading config: ", err)
+	}
+
+	fmt.Printf("%+v\n", userConfig)
+	/*
+		%v — prints the struct values, but not the field names (e.g., {postgres://example Rico})
+		%+v — prints the struct values with field names (e.g., {DbURL:postgres://example CurrentUserName:Rico})
+		%#v — prints the Go-syntax representation (e.g., config.Config{DbURL:"postgres://example", CurrentUserName:"Rico"})
+	*/
+
 }
